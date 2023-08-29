147b7bb6c2c996df52dba38e80e2817c4124b014
b5 <sparkle_pony_2000@qri.io>
2023-08-28 21:30:57 -0400

    fix: invalid Package.swift

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
             path: "IrohLib/Sources/IrohLib"),
         .binaryTarget(
             name: "Iroh",
-            path: "https://github.com/n0-computer/iroh-ffi/releases/download/v0.0.6/IrohLib.xcframework.zip"),
-            checksum: "4e612297d935332562ed8038ab6a66bde32dd644daf5f4d4f64e24f3bdf961e8",
+            url: "https://github.com/n0-computer/iroh-ffi/releases/download/v0.0.6/IrohLib.xcframework.zip",
+            checksum: "4e612297d935332562ed8038ab6a66bde32dd644daf5f4d4f64e24f3bdf961e8")
     ]
 )
