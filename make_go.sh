eea72db0cb35c2649ce4f61ec853c7088b7d9260
Kasey <klhuizinga@gmail.com>
2023-11-24 11:51:09 +0100

    adjust make_go.sh file to do black magic

---
+++ make_go.sh
@@ -0,0 +1,27 @@
+set -eu
+
+MODE=""
+if [ "$#" -eq 1 ]; then
+  if [[ $1 == "release" ]]; then
+    MODE="--release"
+  elif [[ $1 != "debug" ]]; then
+    echo "Unknown mode '$1'. Options are 'release' and 'debug'. Defaults to 'debug'"
+    exit
+  fi
+fi
+
+# the path to the new folder we are including
+INCLUDE_PATH="./go/iroh/include"
+IROH_GO_PATH="./go/iroh/*"
+UDL_PATH="./src/iroh.udl"
+OUT_DIR="./go"
+IROH_GO_FILE="./go/iroh/iroh.go"
+
+rm -rf $IROH_GO_PATH
+
+# build iroh-ffi and save the assets to ./go/iroh/include
+cargo build $MODE --target-dir $INCLUDE_PATH
+
+uniffi-bindgen-go $UDL_PATH --out-dir $OUT_DIR
+
+sed -i '' "s/\/\/ #include <iroh.h>/\/\*\n#cgo CFLAGS: -I.\/go\/iroh\/include\n#cgo LDFLAGS: -liroh -L.\/go\/iroh\/include\n#include <iroh.h>\n\/\*/" $IROH_GO_FILE
