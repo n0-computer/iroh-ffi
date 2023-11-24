58bab6b776591d807d9fde67451465d74d154170
Kasey <klhuizinga@gmail.com>
2023-11-24 13:23:15 +0100

    point to the right location...

--- make_go.sh
+++ make_go.sh
@@ -1,9 +1,11 @@
 set -eu
 
 MODE=""
+DIR_NAME="debug"
 if [ "$#" -eq 1 ]; then
   if [[ $1 == "release" ]]; then
     MODE="--release"
+    DIR_NAME="release"
   elif [[ $1 != "debug" ]]; then
     echo "Unknown mode '$1'. Options are 'release' and 'debug'. Defaults to 'debug'"
     exit
@@ -24,4 +26,4 @@ cargo build $MODE --target-dir $INCLUDE_PATH
 
 uniffi-bindgen-go $UDL_PATH --out-dir $OUT_DIR
 
-sed -i '' "s/\/\/ #include <iroh.h>/\/\*\n#cgo CFLAGS: -I.\/go\/iroh\/include\n#cgo LDFLAGS: -liroh -L.\/go\/iroh\/include\n#include <iroh.h>\n\/\*/" $IROH_GO_FILE
+sed -i '' "s/\/\/ #include <iroh.h>/\/\*\n#cgo CFLAGS: -I.\/include\/${DIR_NAME}\n#cgo LDFLAGS: -liroh -L.\/include\/${DIR_NAME}\n#include <iroh.h>\n\*\//" $IROH_GO_FILE
