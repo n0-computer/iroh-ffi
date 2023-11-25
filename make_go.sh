8e36e33eb69cf8d7c1c1955fb79ca8f67b8a850e
Kasey <klhuizinga@gmail.com>
2023-11-25 12:08:54 +0100

    rename module

--- make_go.sh
+++ make_go.sh
@@ -13,17 +13,17 @@ if [ "$#" -eq 1 ]; then
 fi
 
 # the path to the new folder we are including
-INCLUDE_PATH="./go/iroh/include"
-IROH_GO_PATH="./go/iroh/*"
+GO_DIR="./iroh-go"
+INCLUDE_PATH="${GO_DIR}/iroh/include"
+IROH_GO_PATH="${GO_DIR}/iroh/*"
 UDL_PATH="./src/iroh.udl"
-OUT_DIR="./go"
-IROH_GO_FILE="./go/iroh/iroh.go"
+IROH_GO_FILE="${GO_DIR}/iroh/iroh.go"
 
 rm -rf $IROH_GO_PATH
 
 # build iroh-ffi and save the assets to ./go/iroh/include
 cargo build $MODE --target-dir $INCLUDE_PATH
 
-uniffi-bindgen-go $UDL_PATH --out-dir $OUT_DIR
+uniffi-bindgen-go $UDL_PATH --out-dir $GO_DIR
 
 sed -i '' "s/\/\/ #include <iroh.h>/\/\*\n#cgo CFLAGS: -I.\/include\/${DIR_NAME}\n#cgo LDFLAGS: -liroh -L.\/include\/${DIR_NAME}\n#include <iroh.h>\n\*\//" $IROH_GO_FILE
