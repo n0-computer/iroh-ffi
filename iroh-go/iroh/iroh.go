497ac1c1654479c35d47952bf3fecdd5440d2b19
Kasey <klhuizinga@gmail.com>
2023-11-30 02:01:12 +0100

    adjust scripts, make sure we only keep needed files for go build

--- iroh-go/iroh/iroh.go
+++ iroh-go/iroh/iroh.go
@@ -1,8 +1,8 @@
 package iroh
 
 /*
-#cgo CFLAGS: -I./include/release
-#cgo LDFLAGS: -liroh -L./include/release
+#cgo CFLAGS: -I./ffi/debug
+#cgo LDFLAGS: -liroh -L./ffi/debug
 #include <iroh.h>
 */
 import "C"
