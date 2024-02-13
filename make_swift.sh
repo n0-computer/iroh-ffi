1a40950705d26a7188dc582fbc81119e72dec4f9
Matt Whiteside <mwhiteside.dev@gmail.com>
2024-02-12 20:03:27 -0800

    Add macos-arm64 support. (#92)

--- make_swift.sh
+++ make_swift.sh
@@ -6,6 +6,7 @@ set -eu
 UDL_NAME="iroh"
 FRAMEWORK_NAME="Iroh"
 SWIFT_INTERFACE="IrohLib"
+INCLUDE_DIR="include/apple"
 
 # Compile the rust
 echo "Building aarch64-apple-ios"
@@ -14,24 +15,29 @@ echo "Building aarch64-apple-ios-sim"
 cargo build --release --target aarch64-apple-ios-sim
 echo "Building x86_64-apple-ios"
 cargo build --release --target x86_64-apple-ios
+echo "Building aarch64-apple-darwin"
+cargo build --release --target aarch64-apple-darwin
 
 # Remove old files if they exist
 IOS_ARM64_FRAMEWORK="$FRAMEWORK_NAME.xcframework/ios-arm64/$FRAMEWORK_NAME.framework"
 IOS_SIM_FRAMEWORK="$FRAMEWORK_NAME.xcframework/ios-arm64_x86_64-simulator/$FRAMEWORK_NAME.framework"
+MACOS_ARM64_FRAMEWORK="$FRAMEWORK_NAME.xcframework/macos-arm64/$FRAMEWORK_NAME.framework"
 
 rm -f "$IOS_ARM64_FRAMEWORK/$FRAMEWORK_NAME"
 rm -f "$IOS_ARM64_FRAMEWORK/Headers/${UDL_NAME}FFI.h"
 rm -f "$IOS_SIM_FRAMEWORK/$FRAMEWORK_NAME"
 rm -f "$IOS_SIM_FRAMEWORK/Headers/${UDL_NAME}FFI.h"
+rm -f "$MACOS_ARM64_FRAMEWORK/$FRAMEWORK_NAME"
+rm -f "$MACOS_ARM64_FRAMEWORK/Headers/${UDL_NAME}FFI.h"
 
 rm -f ./target/universal.a
-rm -f include/ios/*
+rm -f $INCLUDE_DIR/*
 
 # Make dirs if it doesn't exist
-mkdir -p include/ios
+mkdir -p $INCLUDE_DIR
 
 # UniFfi bindgen
-cargo run --bin uniffi-bindgen generate "src/$UDL_NAME.udl" --language swift --out-dir ./include/ios
+cargo run --bin uniffi-bindgen generate "src/$UDL_NAME.udl" --language swift --out-dir ./$INCLUDE_DIR
 
 # Make fat lib for sims
 lipo -create \
@@ -44,18 +50,22 @@ cp "./target/aarch64-apple-ios/release/lib${UDL_NAME}.a" \
     "$IOS_ARM64_FRAMEWORK/$FRAMEWORK_NAME"
 cp ./target/universal.a \
     "$IOS_SIM_FRAMEWORK/$FRAMEWORK_NAME"
+cp "./target/aarch64-apple-darwin/release/lib${UDL_NAME}.a" \
+    "$MACOS_ARM64_FRAMEWORK/$FRAMEWORK_NAME"
 
 # Move headers
-cp "include/ios/${UDL_NAME}FFI.h" \
+cp "$INCLUDE_DIR/${UDL_NAME}FFI.h" \
     "$IOS_ARM64_FRAMEWORK/Headers/${UDL_NAME}FFI.h"
-cp "include/ios/${UDL_NAME}FFI.h" \
+cp "$INCLUDE_DIR/${UDL_NAME}FFI.h" \
     "$IOS_SIM_FRAMEWORK/Headers/${UDL_NAME}FFI.h"
+cp "$INCLUDE_DIR/${UDL_NAME}FFI.h" \
+    "$MACOS_ARM64_FRAMEWORK/Headers/${UDL_NAME}FFI.h"
 
 # Move swift interface
-sed "s/${UDL_NAME}FFI/$FRAMEWORK_NAME/g" "include/ios/$UDL_NAME.swift" > "include/ios/$SWIFT_INTERFACE.swift"
+sed "s/${UDL_NAME}FFI/$FRAMEWORK_NAME/g" "$INCLUDE_DIR/$UDL_NAME.swift" > "$INCLUDE_DIR/$SWIFT_INTERFACE.swift"
 
 rm -f "$SWIFT_INTERFACE/Sources/$SWIFT_INTERFACE/$SWIFT_INTERFACE.swift"
-cp "include/ios/$SWIFT_INTERFACE.swift" \
+cp "$INCLUDE_DIR/$SWIFT_INTERFACE.swift" \
     "$SWIFT_INTERFACE/Sources/$SWIFT_INTERFACE/$SWIFT_INTERFACE.swift"
 
 rm -rf "$SWIFT_INTERFACE/artifacts/*"
