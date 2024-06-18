9f0b49e354bea7e551896a4dbc5f40091015f8c2
Asmir Avdicevic <asmir.avdicevic64@gmail.com>
2024-06-18 23:17:31 +0200

    platform specific libs

--- test_kotlin.sh
+++ test_kotlin.sh
@@ -2,6 +2,24 @@ set -eu
 
 # $CLASSPATH must include `jna`
 
+LIB_EXTENSION=""
+
+case "$TEST_OS" in
+    "mac")
+        LIB_EXTENSION=".dylib"
+        ;;
+    "linux")
+        LIB_EXTENSION=".so"
+        ;;
+    "windows")
+        LIB_EXTENSION=".lib"
+        ;;
+    *)
+        echo "Unknown OS specified in TEST_OS"
+        exit 1
+        ;;
+esac
+
 echo "building library"
 cargo build --lib
 
@@ -11,7 +29,7 @@ rm -rf ./kotlin/n0
 cargo run --bin uniffi-bindgen generate "src/iroh.udl" --language kotlin --out-dir ./kotlin --config uniffi.toml
 
 # copy cdylib to outdir
-cp ./target/debug/libiroh.dylib ./kotlin/libuniffi_iroh.dylib
+cp ./target/debug/libiroh.$LIB_EXTENSION ./kotlin/libuniffi_iroh.$LIB_EXTENSION
 
 # Build jar file
 echo "building jar"
